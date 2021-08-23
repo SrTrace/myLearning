b1aff52bae8dc7bdb294607d4b36857cd290b4c5
Sergey <levashov04sergey@gmail.com>
2021-08-23 10:36:21 +0300

    replace to another repo

---
+++ react/react-fund-course/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {BrowserRouter, Route} from "react-router-dom";
+import About from "./pages/About";
+import Posts from "./pages/Posts";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Route path="/about">
+                <About/>
+            </Route>
+            <Route path="/posts">
+                <Posts/>
+            </Route>
+
+        </BrowserRouter>
+    )
+}
+
+export default App;
